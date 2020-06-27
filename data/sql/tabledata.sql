fd4f10e86b76d7497131254523300c3ce7498b37
Hisakazu KATO <hisakazu.katou@daikin.co.jp>
2020-06-27 21:36:29 +0900

    動作確認用データ追加

---
+++ data/sql/tabledata.sql
@@ -0,0 +1,8 @@
+INSERT INTO items(`title`, `content`, `status`, `created`, `updated`) VALUES
+('WEB Application作成','NUXT.js+ElementUIでフロントエンドのWEBアプリを作成する', 'DONE', NOW(), NOW()),
+('RESTful API作成','Spring Bootを用いてRESTful APIを構築する', 'DONE', NOW(), NOW()),
+('フロントエンドとバックエンドを結合','NUXTのアプリからaxios経由でREST APIをコールしフロントとバックエンドを繋げる', 'PROGRESS', NOW(), NOW()),
+('MySQLのDockerイメージを作成する','DockerでRESTful APIでアクセスするMySQLのイメージを作成する', 'PROGRESS', NOW(), NOW()),
+('バックエンドのDockerイメージを作成する','Javaのイメージにバックエンドのjarをレイヤー化したイメージを作成する', 'TODO', NOW(), NOW()),
+('フロントエンドのDockerイメージを作成する','Node.jsからNUXTをインストールしWEBアプリをコピー後にbuildするイメージを作成する', 'TODO', NOW(), NOW()),
+('Docker-composeでパッケージ化する','フロントエンド・バックエンド・DBの3層をひとつのパッケージにまとめる', 'TODO', NOW(), NOW());
